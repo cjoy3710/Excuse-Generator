3247eea4e0b7fd394eeecf72f3f8cff37b4d1aaf
cjoy3710 <cathycathyjoyjoy@gmail.com>
2020-09-26 00:51:25 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,38 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+let generateExcuse = () => {
+  let pronoun = ["a", "my"];
+  let subject = [" third-cousin", " car", " truck", " bakery", " magician"];
+  let action = [
+    " melted",
+    " got stranded",
+    " broke down",
+    " froze",
+    " flew away"
+  ];
+  let where = [
+    " on the highway",
+    " at the football stadium",
+    " in a forrest",
+    " on my roof"
+  ];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    "  " +
+    subject[subjectIndex] +
+    "  " +
+    action[actionIndex] +
+    "  " +
+    where[whereIndex]
+  );
 };
